c514be1a4c9e739a3c81dcc3ded088f70eb4550f
moran-e4 <moran-e4@ulster.ac.uk>
2024-12-23 21:26:22 +0000

    End of 13.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { MoviesComponent } from './movies.component';
 import jsonData from '../assets/titlesplaceholder.json';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [MoviesComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.css'
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home.component';
+import { MoviesComponent } from './movies.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'movies',
+    component: MoviesComponent
+  }
+];
