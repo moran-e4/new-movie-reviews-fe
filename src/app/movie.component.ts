595e395a4aa59538b9f823151c3620ce09a395aa
Eoin Moran <moran-e4@ulster.ac.uk>
2025-01-08 23:34:39 +0000

    14.2 Using coordinate data

--- src/app/movie.component.ts
+++ src/app/movie.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet, ActivatedRoute } from '@angular/router';
 import { DataService } from './data.service';
 import { CommonModule } from '@angular/common';
+import { GoogleMapsModule} from '@angular/google-maps';
 
 
 @Component({
@@ -12,12 +13,17 @@ import { CommonModule } from '@angular/common';
   styleUrl: './movie.component.css',
   imports: [
     RouterOutlet,
-    CommonModule
+    CommonModule,
+    GoogleMapsModule,
   ],
 })
 export class MovieComponent {
 
   movies_list: any;
+  movies_lat: any;
+  movies_lng: any;
+  map_options: google.maps.MapOptions = {};
+  map_locations: any[] = [];
 
   constructor( public dataService: DataService, private route: ActivatedRoute ) {
   }
@@ -25,5 +31,21 @@ export class MovieComponent {
   ngOnInit() {
     this.movies_list = this.dataService.getMovie(
       this.route.snapshot.paramMap.get('tconst'));
+    this.movies_lat = this.movies_list[0].location.coordinates[0];
+    this.movies_lng = this.movies_list[0].location.coordinates[1];
+
+    this.map_locations.push({
+      lat: this.movies_lat,
+      lng: this.movies_lng,
+    });
+
+    this.map_options = {
+      mapId: "DEMOMAP",
+      center: {
+        lat: this.movies_lat,
+        lng: this.movies_lng,
+      },
+      zoom: 13
+    }
   }
 }
