7207232f7affffe64c5a7878db8897eafd4488cc
Eoin Moran <moran-e4@ulster.ac.uk>
2025-01-10 00:26:41 +0000

    styling

--- src/app/web.service.ts
+++ src/app/web.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class WebService {
-  pageSize: number = 5;
+  pageSize: number = 12;
 
   constructor(private http: HttpClient) { }
 
